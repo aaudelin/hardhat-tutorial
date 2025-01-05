9c50b57dd429423cdd7e5ea9720cf8572123eb42
Aurélien Audelin <aurelien.audelin@gmail.com>
2025-01-05 15:11:03 +0100

    feat: add console.log

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,6 +4,7 @@
 // It will be used by the Solidity compiler to validate its version.
 pragma solidity ^0.8.0;
 
+import "hardhat/console.sol";
 
 // This is the main building block for smart contracts.
 contract Token {
@@ -49,6 +50,13 @@ contract Token {
         // Transfer the amount.
         balances[msg.sender] -= amount;
         balances[to] += amount;
+
+        console.log(
+            "Transferring from %s to %s %s tokens",
+            msg.sender,
+            to,
+            amount
+        );
 
         // Notify off-chain applications of the transfer.
         emit Transfer(msg.sender, to, amount);
